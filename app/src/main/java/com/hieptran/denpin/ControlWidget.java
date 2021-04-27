c88da9e41da6c4df9fe2e191a75fff821b705ce8
Trần Đại Hiệp <daihieptn97@gmail.com>
2021-04-27 21:06:07 +0700

    init

---
+++ app/src/main/java/com/hieptran/denpin/ControlWidget.java
@@ -0,0 +1,76 @@
+package com.hieptran.denpin;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class ControlWidget extends AppWidgetProvider {
+
+    private static final String ACTION_CLICK_CONTROL = "actionClickControl";
+
+    void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                         int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.control_widget);
+//        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+
+        views.setOnClickPendingIntent(R.id.btnWidgetControl, getPendingSelfIntent(context, ACTION_CLICK_CONTROL));
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        String action = intent.getAction();
+        Log.e(getClass().getSimpleName(), "onReceive:" + action);
+
+        if (action == null) {
+            Log.e(getClass().getSimpleName(), "action null");
+            return;
+        }
+
+
+        switch (action) {
+            case ACTION_CLICK_CONTROL:
+
+                break;
+        }
+    }
+
+    private PendingIntent getPendingSelfIntent(Context context, String action) {
+        Intent intent = new Intent(context, getClass());
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
+    }
+
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
